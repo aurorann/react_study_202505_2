685b3f510fcb8409b3deecdfd3f59f8d99e0af05
keson <keson>
2025-05-12 17:41:51 +0900

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Profile from "./Gallery.jsx";
+import Packing from "./PackingList.jsx"
+
+export default function App() {
+    return (
+        <>
+            {/*<Profile/>*/}
+            <Packing/>
+        </>
+    );
+}
---
+++ src/Avatar.jsx
@@ -0,0 +1,16 @@
+import { getImageUrl } from "./utils.jsx";
+
+export default function Avatar({ person, size }) {
+    return (
+        <>
+        <img
+            className="avatar"
+            src={getImageUrl(person)}
+            alt={person.name}
+            width={size}
+            height={size}
+        />
+        {person.name}
+        </>
+    );
+}
