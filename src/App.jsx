d655d9a9a30c5630f0a06c9847139d9d1945bf1f
keson <keson>
2025-05-14 09:49:12 +0900

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Profile from "./Gallery.jsx";
 import Packing from "./PackingList.jsx"
+import People from "./People.jsx"
+import Recipes from "./Recipes.jsx"
 
 export default function App() {
     return (
         <>
             {/*<Profile/>*/}
-            <Packing/>
+            {/*<Packing/>*/}
+            {/*<People/>*/}
+            <Recipes/>
         </>
     );
 }
